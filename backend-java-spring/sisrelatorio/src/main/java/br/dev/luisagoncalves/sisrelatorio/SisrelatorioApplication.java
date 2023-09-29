f783b265975453510ef44df509624bd5d400c48d
Luísa O. Gonçalves <luisaolg20@gmail.com>
2023-09-28 23:42:45 -0300

    configuração inicial do back-end

---
+++ backend-java-spring/sisrelatorio/src/main/java/br/dev/luisagoncalves/sisrelatorio/SisrelatorioApplication.java
@@ -0,0 +1,13 @@
+package br.dev.luisagoncalves.sisrelatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisrelatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisrelatorioApplication.class, args);
+	}
+
+}
